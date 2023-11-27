42f3817f66dd53da6b600e7e41e845c0681cad21
Jake Nelson <jake@couldbejake.io>
2023-11-27 00:52:55 +0000

    Update main.js

--- main.js
+++ main.js
@@ -338,5 +338,5 @@ get GPT to write psuedo code, then create functions that should call each other.
 // maybe start program with human<->gpt interaction, and see what to create from there
 
 
-Max tasks page
-Objectives trigger changing of tasks
+// Max tasks page
+// Objectives trigger changing of tasks
